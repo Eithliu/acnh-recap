8d4a35b181ea9a863f87b3e44964c1981041cc3d
Eithliu <iris.benoit@gmail.com>
2022-08-25 13:33:04 +0200

    utilisation de grid

--- acnh-app/src/components/HelloWorld.vue
+++ acnh-app/src/components/HelloWorld.vue
@@ -8,32 +8,6 @@
         <li>Saying: <em>{{ villager.saying }}</em></li>
       </ul>
     </div>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
@@ -74,7 +48,7 @@ a {
   color: #42b983;
 }
 .cards {
-  box-shadow: 2px 2px 5px #f2f2f2;
+  box-shadow: 2px 2px 5px #afafaf;
   border-radius: 10px;
   padding-bottom: 20px;
 }
@@ -82,9 +56,11 @@ ul.cards{
   display: flex;
   flex-direction: column;
 }
-.grille {
+div.grille {
   display: grid;
-  grid-template-columns: fit-content(100%);
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+  grid-auto-rows: minmax(100px, auto);
 }
 ul > img.img-card {
   width: 30%;
